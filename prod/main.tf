32b071cdfd8339c95df1066fb3ffbb65385012d4
Zeeshan <zeeshanlive09@gmail.com>
2020-07-23 11:07:33 +0100

    added prod

---
+++ prod/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "my_vpc" {
+    source= "../modules/vpc"
+    vpc_cidr = "13.0.0.0/16"
+    env = "dev"
+    subnet_cidr1 = "13.0.1.0/24"
+    subnet_cidr2 = "13.0.2.0/24"
+    vpcid= module.my_vpc.ukvpc
+}
