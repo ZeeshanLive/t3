416cdfbab8c8c207ed8a2ad8729c716cb6846262
Zeeshan <zeeshanlive09@gmail.com>
2020-07-23 11:10:21 +0100

    added prod tag

--- prod/main.tf
+++ prod/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "my_vpc" {
     source= "../modules/vpc"
     vpc_cidr = "13.0.0.0/16"
-    env = "dev"
+    env = "prod"
     subnet_cidr1 = "13.0.1.0/24"
     subnet_cidr2 = "13.0.2.0/24"
     vpcid= module.my_vpc.ukvpc
